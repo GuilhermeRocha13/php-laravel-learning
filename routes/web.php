112a4399844bf470f528a4b63707b57e6d0457ea
Guilherme Henrique <guilherme_h2006@hotmail.com>
2022-12-27 09:19:23 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,14 @@ use Illuminate\Support\Facades\Route;
 
  Route::get('/contato', 'ContatoController@Contato');
 
+ Route::get(
+    '/contato/{nome}/{categoria_id}' , //
+    function (
+        string $nome = 'Desconhecido',
+        int $categoria_id = 1
+    ) {
+        echo "Estamos aqui: $nome - $categoria_id";
+    }
+) ->where('categoria_id' , '[0-9]+'); where('nome' , '[A-Z a-z]');
+
 
