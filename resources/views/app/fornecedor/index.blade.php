12c06630c3733e327fea60f0f8f38464c7605df4
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-01-02 17:11:26 -0300

    a

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,17 @@
+<h3> fornec </h3>
+
+{{-- assim que é feito comentarios na blade--}}
+
+@php
+//esse trecho sera php puro, entao o comentario é feito daquela forma padrao 
+/*
+*/
+@endphp
+
+
+@php
+ 
+
+@endphp
+
+{{ $fornecedores }}
