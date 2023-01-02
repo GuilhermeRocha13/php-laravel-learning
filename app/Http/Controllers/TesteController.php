d05156c167fc88fa64319649d2472bd5ed6e4656
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-01-02 10:14:10 -0300

    teste controller

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2){
-        echo "jesus é totalmente poderoso $p1, $p2";
+
+        //echo "jesus é totalmente poderoso $p1, $p2";
+
+        //return view('site.teste', compact('p1', 'p2'));  No compact as variaveis são armazenadas nessa função, é a forma mais enxuta 
+
+        return view('site.teste')->with('p1', $p1) ->with('p2', $p2);    //Nessa forma terá que puxar essa função para cada variavel
 
     }
 }
