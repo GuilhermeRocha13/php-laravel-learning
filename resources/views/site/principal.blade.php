c518e982dac07193ff16ce90451a3def231f1f0e
Guilherme Henrique <guilherme_h2006@hotmail.com>
2023-01-26 13:27:29 -0300

    section

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
-    </head>
+@extends('site.layout.basico')
 
     <body>
         <div class="topo">
